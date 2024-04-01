3068df36d70768faf78b3265a3fd7a2034188a02
Mohamed <rifaeeckm@gmail.com>
2024-04-01 12:06:18 +0530

    doctor profile remove sm bugs

--- frontend/src/components/User/DoctorProfile.jsx
+++ frontend/src/components/User/DoctorProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
-import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import {  useLocation, useNavigate, useParams } from "react-router-dom";
 import axios from "../../axios";
 
 const DoctorProfile = () => {
